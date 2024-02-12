930180125b1fb95462d134ed5cb69aa8ff6b1345
Aznague <latifaaznag810@gmail.com>
2024-02-12 20:20:37 +0100

    Premier commit

---
+++ resources/views/Acceuil.blade.php
@@ -0,0 +1,589 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <title>Desktop - 1</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Itim%3A400"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A100%2C400%2C800%2C900"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter%3A800"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat%3A100%2C400%2C900"/>
+  <link rel="stylesheet" href="./styles/desktop-1.css"/>
+  <style>html {
+	font-size:66%;
+} 
+* {
+	margin: 0;
+	padding: 0;
+}
+ul, li {
+	list-style: none;
+}
+input {
+	border: none;
+}
+body {
+  width: 144rem;
+}.desktop-1-vZ1 {
+  overflow: hidden;
+
+  flex-direction: column;
+  background-color: #000000;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu {
+  margin-bottom: 2.2rem;
+  width: 100%;
+  height: 78.2rem;
+  display: flex;
+  align-items: flex-end;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX {
+  margin-right: 106.3rem;
+  width: 144rem;
+  height: 100%;
+  background-size: 102.708% 106.386%;
+  background-position: -2rem -2.4969rem;
+  background-image: url('../assets/business-guest-speaker-collab-youtube-thumbnail-1-bg.png');
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b {
+  box-sizing: border-box;
+  padding-right: 5.5rem;
+  width: 143.4rem;
+  height: 77.8rem;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  background-color: rgba(0, 0, 0, 0.6600000262);
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .mediablend6-removebg-preview-1-kro {
+  width: 29.7rem;
+  height: 15.1rem;
+  object-fit: cover;
+  vertical-align: top;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-7-exB {
+  margin: 29rem 7.8rem 22.5rem 0rem;
+  box-sizing: border-box;
+  padding: 3.7rem 4.6rem 1.5rem 7.3rem;
+  height: calc(100% - 51.5rem);
+  overflow: hidden;
+  position: relative;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  border: solid 0.3rem #ff0000;
+  border-radius: 5rem;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-7-exB .auto-group-wepd-WDh {
+  margin: 0rem 0rem 0.2rem 0.5rem;
+  width: 28.8rem;
+  height: 16.4rem;
+  position: relative;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-7-exB .auto-group-wepd-WDh .adulte-r4b {
+  width: 28.8rem;
+  height: 12rem;
+  position: absolute;
+  left: 0;
+  top: 0;
+  font-size: 10rem;
+  font-weight: 400;
+  line-height: 1.2;
+  color: #ffffff;
+  font-family: Itim, 'Source Sans Pro';
+  white-space: nowrap;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-7-exB .auto-group-wepd-WDh .dh-month-4gT {
+  width: 21.9rem;
+  height: 4.8rem;
+  position: absolute;
+  left: 4.3rem;
+  top: 11.6rem;
+  font-size: 4rem;
+  font-weight: 400;
+  line-height: 1.2;
+  color: #ffffff;
+  font-family: Itim, 'Source Sans Pro';
+  white-space: nowrap;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-7-exB .auto-group-gmtw-MfZ {
+  margin-right: 3.5rem;
+  width: 25.8rem;
+  height: 4.5rem;
+  font-size: 2.4rem;
+  font-weight: 800;
+  line-height: 1.2125;
+  color: #ffffff;
+  font-family: Inter, 'Source Sans Pro';
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url('../assets/frame-8-7oq.png');
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-8-N4s {
+  margin: 29rem 7.7rem 22.5rem 0rem;
+  box-sizing: border-box;
+  padding: 3.7rem 4.6rem 1.5rem 7.3rem;
+  width: 41.2rem;
+  height: calc(100% - 51.5rem);
+  overflow: hidden;
+  position: relative;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  border: solid 0.3rem #00c1ff;
+  border-radius: 5rem;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-8-N4s .auto-group-wbvj-T6K {
+  width: 100%;
+  height: 16.6rem;
+  position: relative;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-8-N4s .auto-group-wbvj-T6K .enfan-QXM {
+  width: 29.3rem;
+  height: 12rem;
+  position: absolute;
+  left: 0;
+  top: 0;
+  font-size: 10rem;
+  font-weight: 400;
+  line-height: 1.2;
+  color: #ffffff;
+  font-family: Itim, 'Source Sans Pro';
+  white-space: nowrap;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-8-N4s .auto-group-wbvj-T6K .dh-month-iY3 {
+  width: 22.2rem;
+  height: 4.8rem;
+  position: absolute;
+  left: 1.9rem;
+  top: 11.8rem;
+  font-size: 4rem;
+  font-weight: 400;
+  line-height: 1.2;
+  color: #ffffff;
+  font-family: Itim, 'Source Sans Pro';
+  white-space: nowrap;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .frame-8-N4s .auto-group-s9mm-o3h {
+  margin-right: 3.5rem;
+  width: 25.8rem;
+  height: 4.5rem;
+  font-size: 2.4rem;
+  font-weight: 800;
+  line-height: 1.2125;
+  color: #ffffff;
+  font-family: Inter, 'Source Sans Pro';
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url('../assets/frame-8.png');
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .frame-28-U8b .connecter-FgP {
+  margin-top: 2.7rem;
+  font-size: 2.4rem;
+  font-weight: 800;
+  line-height: 1.2125;
+  color: #00c1ff;
+  font-family: Inter, 'Source Sans Pro';
+  white-space: nowrap;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .auto-group-qysm-ZGX .auto-group-qysm-ZGX-bg {
+  width: 144rem;
+  height: 78.2rem;
+  position: relative;
+  background-image: url('REPLACE_IMAGE:b09f440e35e7b932c444ffa8b2517caa6fd3e8d4');
+}
+.desktop-1-vZ1 .auto-group-rqit-XHu .love-and-other-words-de-christina-lauren-1-G5h {
+  margin-bottom: 6.2rem;
+  width: 33.5rem;
+  height: 40rem;
+  object-fit: cover;
+  vertical-align: top;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-9-mo9 {
+  margin: 0rem 0rem 1.3rem 0.5rem;
+  width: 143.5rem;
+  height: 33.2rem;
+  position: relative;
+  overflow: hidden;
+  background-color: #000000;
+  background: linear-gradient(180deg, rgba(55, 55, 55, 0.200000003) -0%, rgba(0, 0, 0, 0.200000003) 100%);
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu {
+  box-sizing: border-box;
+  padding: 5.1rem 2.7rem 0rem 2.3rem;
+  width: 137rem;
+  height: 52.9rem;
+  position: absolute;
+  left: 5.9rem;
+  top: 1.7rem;
+  overflow: hidden;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .profitez-de-votre-temps-avec-yPR {
+  margin: 0rem 9.4rem 2.8rem 0rem;
+  font-size: 8rem;
+  font-weight: 900;
+  line-height: 1.0299999237;
+  letter-spacing: 0.16rem;
+  color: #ffffff;
+  font-family: Montserrat, 'Source Sans Pro';
+  white-space: nowrap;
+  display: flex;
+  align-items: baseline;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .profitez-de-votre-temps-avec-yPR .profitez-de-votre-temps-avec-yPR-sub-0 {
+  font-size: 8rem;
+  font-weight: 900;
+  line-height: 1.0299999237;
+  letter-spacing: 0.16rem;
+  color: #ffffff;
+  font-family: Montserrat, 'Source Sans Pro';
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .profitez-de-votre-temps-avec-yPR .profitez-de-votre-temps-avec-yPR-sub-1 {
+  font-size: 8rem;
+  font-weight: 900;
+  line-height: 1.0299999237;
+  letter-spacing: 0.16rem;
+  color: #ffffff;
+  font-family: Montserrat, 'Source Sans Pro';
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .profitez-de-votre-temps-avec-yPR .profitez-de-votre-temps-avec-yPR-sub-2 {
+  font-size: 8rem;
+  font-weight: 900;
+  line-height: 1.0299999237;
+  letter-spacing: 0.16rem;
+  color: #ffffff;
+  font-family: Montserrat, 'Source Sans Pro';
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .auto-group-3dpz-dF1 {
+  margin: 0rem 30.3rem 10.2rem 27.5rem;
+  width: calc(100% - 57.8rem);
+  height: 10.3rem;
+  position: relative;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .auto-group-3dpz-dF1 .media-9UF {
+  width: 40.3rem;
+  height: 10.3rem;
+  position: absolute;
+  left: 0;
+  top: 0;
+  font-size: 10rem;
+  font-weight: 900;
+  line-height: 1.03;
+  letter-spacing: 0.2rem;
+  font-family: Montserrat, 'Source Sans Pro';
+  white-space: nowrap;
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .auto-group-3dpz-dF1 .blend-TUw {
+  width: 38rem;
+  height: 10.3rem;
+  position: absolute;
+  left: 36.2rem;
+  top: 0;
+  font-size: 10rem;
+  font-weight: 900;
+  line-height: 1.03;
+  letter-spacing: 0.2rem;
+  font-family: Montserrat, 'Source Sans Pro';
+  white-space: nowrap;
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-10-hRu .frame-11-9sZ {
+  margin-left: 77.1rem;
+  width: 54.9rem;
+  height: 84.6rem;
+  overflow: hidden;
+  position: relative;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-9-mo9 .frame-12-h8P {
+  width: 87.7rem;
+  height: 87.4rem;
+  position: absolute;
+  left: 0;
+  top: 30.6rem;
+  overflow: hidden;
+}
+.desktop-1-vZ1 .frame-27-aT5 {
+  margin-bottom: 6.9rem;
+  box-sizing: border-box;
+  padding: 7.3rem 3.9rem 0.045rem 4.2rem;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  background-color: #000000;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .mediablend-pour-vous-et-pour-vos-enfants-T15 {
+  margin: 0rem 0.9rem 10.9rem 0rem;
+  font-size: 4rem;
+  font-weight: 400;
+  line-height: 1.0299999237;
+  letter-spacing: 0.08rem;
+  font-style: italic;
+  text-decoration-line: underline;
+  color: #c93333;
+  font-family: Montserrat, 'Source Sans Pro';
+  white-space: nowrap;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb {
+  width: 100%;
+  height: 128.3545rem;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-bxh5-TvB {
+  margin: 0rem 8.05rem 11.974rem 0rem;
+  align-items: flex-end;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-bxh5-TvB .it-2-Csm {
+  margin: 0rem 0.909rem 10.542rem 0rem;
+  width: 35.1913rem;
+  height: 51.9576rem;
+  object-fit: cover;
+  vertical-align: top;
+  border-radius: 2rem;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-bxh5-TvB .tlcharger-7-1-WtT {
+  width: 35rem;
+  height: 53.8801rem;
+  object-fit: cover;
+  vertical-align: top;
+  border-radius: 2rem;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-pgpm-EZZ {
+  margin: 9.2rem 8.15rem 17.755rem 0rem;
+  height: calc(100% - 26.9545rem);
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-pgpm-EZZ .sur-notre-plateforme-dcouvrez-une-large-gamme-de-filmsque-vous-pouvez-regarder-en-streaming-ou-tlcharger-pour-une-exprience-cinmatographique-optimale-xEf {
+  margin: 0rem 0rem 23.1rem 1.7rem;
+  max-width: 46.9rem;
+  text-align: center;
+  font-size: 4rem;
+  font-weight: 100;
+  line-height: 1.0299999237;
+  letter-spacing: 0.08rem;
+  color: #ffffff;
+  font-family: Montserrat, 'Source Sans Pro';
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-pgpm-EZZ .explorez-de-nouvelles-histoires-sur-mediablend-avec-une-slection-varie-de-livres-neufs-et-doccasion-facilitant-lachat-et-la-vente-grce-une-interface-conviviale-et-des-annonces-dtailles-ZVM {
+  margin-right: 1.7rem;
+  max-width: 46.9rem;
+  text-align: center;
+  font-size: 4rem;
+  font-weight: 100;
+  line-height: 1.0299999237;
+  letter-spacing: 0.08rem;
+  color: #ffffff;
+  font-family: Montserrat, 'Source Sans Pro';
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-jc2x-Mw1 {
+  margin-top: 3.8rem;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-jc2x-Mw1 .normal-people-by-sally-rooney-mum-of-three-world-1-WZ1 {
+  margin-bottom: 12.3778rem;
+  width: 35rem;
+  height: 58.7222rem;
+  position: relative;
+  background-size: 125.812% 100%;
+  background-position: -3.8888rem 0rem;
+  background-image: url('../assets/normal-people-by-sally-rooney-mum-of-three-world-1.png');
+  border-radius: 2rem;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-27-aT5 .auto-group-oet5-ixb .auto-group-jc2x-Mw1 .wir-sind-hier-ein-kinderbuch-ber-flucht-hoffnung-1-xR1 {
+  width: 35rem;
+  height: 53.4545rem;
+  position: relative;
+  background-size: 114.463% 99.898%;
+  background-position: 0 0.0544rem;
+  background-image: url('../assets/wir-sind-hier-ein-kinderbuch-ber-flucht-hoffnung-1.png');
+  border-radius: 2rem;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-26-cVZ {
+  box-sizing: border-box;
+  padding: 33.9rem 2rem 0rem 0rem;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0.5600000024) -0%, rgba(202, 170, 170, 0.5600000024) 100%);
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-26-cVZ .auto-group-ig67-9Eb {
+  margin-right: 98.9rem;
+  box-sizing: border-box;
+  padding: 12.2rem 9.9rem 5.5rem 4.5rem;
+  width: 39.5rem;
+  height: 22.3rem;
+  display: flex;
+  align-items: center;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url('../assets/media-blend-11-1-bg.png');
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-26-cVZ .auto-group-ig67-9Eb .facebook-1-1-3as {
+  margin: 0rem 6.6rem 0.6rem 0rem;
+  width: 4rem;
+  height: 4rem;
+  object-fit: cover;
+  vertical-align: top;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-26-cVZ .auto-group-ig67-9Eb .youtube-1-Zp7 {
+  margin-right: 5.7rem;
+  width: 4.6rem;
+  height: 4.6rem;
+  object-fit: cover;
+  vertical-align: top;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-26-cVZ .auto-group-ig67-9Eb .instagram-1-74w {
+  margin-top: 0.4rem;
+  width: 4.2rem;
+  height: 4.2rem;
+  object-fit: cover;
+  vertical-align: top;
+  flex-shrink: 0;
+}
+.desktop-1-vZ1 .frame-26-cVZ .paypallogopng2-1-oiT {
+  margin-bottom: 1.4rem;
+  width: 5.3rem;
+  height: 5.3rem;
+  object-fit: cover;
+  vertical-align: top;
+  flex-shrink: 0;
+}
+a{
+  text-decoration: none;
+}</style>
+</head>
+<body>
+<div class="desktop-1-vZ1">
+  <div class="auto-group-rqit-XHu">
+    <div class="auto-group-qysm-ZGX">
+      <div class="frame-28-U8b">
+        <img class="mediablend6-removebg-preview-1-kro" src="./assets/mediablend6-removebg-preview-1.png"/>
+        <div class="frame-7-exB">
+          <div class="auto-group-wepd-WDh">
+            <p class="adulte-r4b">Adulte</p>
+            <p class="dh-month-4gT">99Dh/month</p>
+          </div>
+          <a href=""><div class="auto-group-gmtw-MfZ">Commancez</div></a>
+        </div>
+        <div class="frame-8-N4s">
+          <div class="auto-group-wbvj-T6K">
+            <p class="enfan-QXM">Enfant</p>
+            <p class="dh-month-iY3">49Dh/month</p>
+          </div>
+          <a href=""><div class="auto-group-s9mm-o3h">Commancez</div></a>
+        </div>
+        <a href=""><p class="connecter-FgP">Connecter</p></a>
+      </div>
+      <div class="auto-group-qysm-ZGX-bg">
+      </div>
+    </div>
+    <img class="love-and-other-words-de-christina-lauren-1-G5h" src="./assets/love-and-other-words-de-christina-lauren-1.png"/>
+  </div>
+  <div class="frame-9-mo9">
+    <div class="frame-10-hRu">
+      <p class="profitez-de-votre-temps-avec-yPR">
+        <span class="profitez-de-votre-temps-avec-yPR-sub-0">Profitez de votre</span>
+        <span class="profitez-de-votre-temps-avec-yPR-sub-1">  </span>
+        <span class="profitez-de-votre-temps-avec-yPR-sub-2">temps avec </span>
+      </p>
+      <div class="auto-group-3dpz-dF1">
+        <p class="media-9UF">MEDIA </p>
+        <p class="blend-TUw"> BLEND</p>
+      </div>
+      <div class="frame-11-9sZ">
+      </div>
+    </div>
+    <div class="frame-12-h8P">
+    </div>
+  </div>
+  <div class="frame-27-aT5">
+    <p class="mediablend-pour-vous-et-pour-vos-enfants-T15">MediaBlend pour vous et pour vos enfants</p>
+    <div class="auto-group-oet5-ixb">
+      <div class="auto-group-bxh5-TvB">
+        <img class="it-2-Csm" src="./assets/it-2.png"/>
+        <img class="tlcharger-7-1-WtT" src="./assets/tlcharger-7-1.png"/>
+      </div>
+      <div class="auto-group-pgpm-EZZ">
+        <p class="sur-notre-plateforme-dcouvrez-une-large-gamme-de-filmsque-vous-pouvez-regarder-en-streaming-ou-tlcharger-pour-une-exprience-cinmatographique-optimale-xEf">Sur notre plateforme, découvrez une large gamme de films,que vous pouvez regarder en streaming ou télécharger pour une expérience cinématographique optimale.</p>
+        <p class="explorez-de-nouvelles-histoires-sur-mediablend-avec-une-slection-varie-de-livres-neufs-et-doccasion-facilitant-lachat-et-la-vente-grce-une-interface-conviviale-et-des-annonces-dtailles-ZVM">
+        Explorez de nouvelles histoires sur MediaBlend avec une sélection variée de livres neufs et
+        <br/>
+         d’occasion, facilitant l&#39;achat et la vente grâce à une interface conviviale et des annonces détaillées.
+        </p>
+      </div>
+      <div class="auto-group-jc2x-Mw1">
+        <div class="normal-people-by-sally-rooney-mum-of-three-world-1-WZ1">
+        </div>
+        <div class="wir-sind-hier-ein-kinderbuch-ber-flucht-hoffnung-1-xR1">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="frame-26-cVZ">
+    <div class="auto-group-ig67-9Eb">
+      <img class="facebook-1-1-3as" src="./assets/facebook-1-1.png"/>
+      <img class="youtube-1-Zp7" src="./assets/youtube-1.png"/>
+      <img class="instagram-1-74w" src="./assets/instagram-1.png"/>
+    </div>
+    <img class="paypallogopng2-1-oiT" src="./assets/paypallogopng2-1.png"/>
+  </div>
+</div>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Acceuil', function () {
+    return view('Acceuil');
+});
